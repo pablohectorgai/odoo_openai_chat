f1d04f720bf9b960c7b5a04e742ca651f4b46762
pablohectorgai <pablohectorgonzalezjr@gmail.com>
2025-11-08 18:23:46 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -5,7 +5,7 @@
     'author': 'MetaProject',
     'license': 'LGPL-3',
     'category': 'Productivity/Discuss',
-    'depends': ["base", "mail", "discuss"],
+    'depends': ["base", "mail"],
     'data': [
         'views/res_config_settings_views.xml',
     ],
