28516e958a64440dea3ba9cb75fb29f953c4d6f0
pablohectorgonzalez <pablohectorgonzalezjr@gmail.com>
2025-11-10 07:27:37 -0600

    Create discuss_channel_member.py

---
+++ models/discuss_channel_member.py
@@ -0,0 +1,33 @@
+# odoo_addons/odoo_openai_chat/models/discuss_channel_member.py
+import logging
+from odoo import models
+
+_logger = logging.getLogger(__name__)
+
+class DiscussChannelMember(models.Model):
+    _inherit = 'discuss.channel.member'
+
+    def write(self, vals):
+        """
+        Cuando el bot de OpenAI publica, evitamos tocar is_pinned y last_interest_dt
+        para no competir con notify_typing y otras actualizaciones concurrentes.
+        """
+        if self.env.context.get('openai_skip_pin'):
+            vals = dict(vals)  # copia
+            removed = []
+            # Campos que originan el choque mostrado en los logs
+            if 'is_pinned' in vals:
+                vals.pop('is_pinned', None)
+                removed.append('is_pinned')
+            if 'last_interest_dt' in vals:
+                vals.pop('last_interest_dt', None)
+                removed.append('last_interest_dt')
+
+            # (Opcional) si observas conflictos con otros campos de interés, puedes filtrarlos aquí:
+            # if 'last_seen_dt' in vals:
+            #     vals.pop('last_seen_dt', None)
+            #     removed.append('last_seen_dt')
+
+            if removed:
+                _logger.debug("openai_skip_pin: filtrando %s en write(%s)", removed, self.ids)
+        return super().write(vals)
